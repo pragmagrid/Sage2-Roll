8e312ef35319e96183fde2a3b530e1e988c492b8
root <root@lima-vc-0.sdsc.optiputer.net>
2018-12-06 16:35:30 -0800

    Replace Sage2 with sage2-server dir

--- src/Sage2/version.mk
+++
@@ -1,5 +0,0 @@
-PKGROOT		= opt
-NAME        = sage2
-VERSION     = 1.0.0
-RELEASE 	= 1
-TARBALL_POSTFIX	= tar.bz2  
---
+++ src/sage2-server/version.mk
@@ -0,0 +1,7 @@
+PKGROOT		= opt
+NAME        = sage2
+RELEASE 	= 1
+TARBALL_POSTFIX	= tar.gz
+
+RPM.FILES = \
+/opt/sage2 
